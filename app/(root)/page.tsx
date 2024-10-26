e61ccd7048b1b685807b1abd5375daecc2459cc2
Han <handriantog@gmail.com>
2024-10-26 22:50:56 +0800

    Update on new device

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import React from "react"
 
 const Home = () => {
-  const loggedIn = { firstName: 'Han' }
+  const loggedIn = { firstName: "Han" }
   return (
     <section className="home">
       <div className="home-content">
@@ -11,7 +11,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guests"}
             subtext="Access and manage your account and transactions efficiently"
           />
         </header>
